48b08ca4c835aac4ec4a2cd5c9f834e1c9d73f35
dennis <denhouston1@icloud.com>
2023-01-29 22:02:17 +0000

    Write to data.json

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+import fs from 'fs'
 import fetch from 'node-fetch';
 
 const url = 'https://www.roh.org.uk/api/event-details?slug=turandot-by-andrei-serban'
@@ -8,5 +9,11 @@ fetch(url)
       console.log('keys', Object.keys(data))
       console.log('data', data.data)
       console.log('included', data.included.length)
+      fs.writeFileSync('./data.json', JSON.stringify(data, null, 2))
+      // console.log('keys', Object.keys(data))
+      // console.log('data', JSON.stringify(data.data, null, 2))
+      // console.log('included', data.included[0])
+
+      const title = data.data.attributes.title
     })
   })
